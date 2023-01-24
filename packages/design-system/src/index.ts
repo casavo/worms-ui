f2a4b171d957b1d813d091fc0064ef7233966d3d
Simone D'Avico <simonedavico@gmail.com>
2023-01-24 15:31:27 +0100

    chore: export theme class and apply worms-ui theme on all stories

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -1,4 +1,5 @@
 export { Button } from './components/Button';
 export { Box } from './components/Box';
+export { themeClass as wormsUiThemeClass } from './theme/theme.css';
 
 export * from './tokens';
