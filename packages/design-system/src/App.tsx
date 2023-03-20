6add6d5aacb42a824c24f86dcca50531e878cdb6
Laura Marina Bianchi <lauramarinabianchi@gmail.com>
2023-03-20 12:41:05 +0100

    chore: try icon Component

--- packages/design-system/src/App.tsx
+++ packages/design-system/src/App.tsx
@@ -3,7 +3,13 @@ import { Box } from './components/Box';
 import { Button } from './components/Button';
 import { Spinner } from './components/Spinner';
 import { themeClass } from './theme/theme.css';
-import { spacings } from './tokens';
+import { colors, spacings } from './tokens';
+
+export const BathroomIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({ fill = colors.greyscale900, ...props }) => (
+  <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" fill={fill} {...props}>
+    <path d="M12 0a4 4 0 0 1 4 4v7.5a3.5 3.5 0 0 1-1.999 3.163L14 15a1 1 0 1 1-2 0H4a1 1 0 1 1-2 0v-.337A3.5 3.5 0 0 1 0 11.5V9a1 1 0 0 1 1-1h13V4a2 2 0 0 0-1.85-1.995L12 2h-1.535c.34.588.535 1.271.535 2v2H3V4a4 4 0 0 1 4-4h5Zm2 10H2v1.5a1.5 1.5 0 0 0 1.356 1.493L3.5 13h9a1.5 1.5 0 0 0 1.493-1.355L14 11.5V10ZM7 2a2 2 0 0 0-1.995 1.85L5 4h4a2 2 0 0 0-1.85-1.995L7 2Z" />
+  </svg>
+);
 
 function App() {
   const [selected, setSelected] = useState(false);
@@ -12,31 +18,8 @@ function App() {
     <div className={themeClass}>
       <Box style={{ padding: spacings.xl, display: 'flex', gap: spacings.m, flexDirection: 'column' }}>
         <Box style={{ display: 'flex', gap: spacings.m, flexDirection: 'row' }}>
-          <Button
-            icon={(props) => (
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M8.0058 1.33325L14.6668 5.03696V14.6666H1.3335V5.03696L8.0058 1.33325ZM8.00516 3.23909L3.00016 6.01742V12.9999H13.0002V6.01659L8.00516 3.23909Z"
-                />
-              </svg>
-            )}
-          >
-            Primary button
-          </Button>
-          <Button
-            disabled
-            icon={(props) => (
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M8.0058 1.33325L14.6668 5.03696V14.6666H1.3335V5.03696L8.0058 1.33325ZM8.00516 3.23909L3.00016 6.01742V12.9999H13.0002V6.01659L8.00516 3.23909Z"
-                />
-              </svg>
-            )}
-          >
+          <Button icon={(props) => <BathroomIcon {...props} />}>Primary button</Button>
+          <Button disabled icon={(props) => <BathroomIcon {...props} />}>
             Disabled button
           </Button>
           <Button loading>Loading button</Button>
@@ -44,33 +27,13 @@ function App() {
         <Box style={{ display: 'flex', gap: spacings.m, flexDirection: 'row' }}>
           <Button
             iconPosition="right"
-            icon={(props) => (
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M8.0058 1.33325L14.6668 5.03696V14.6666H1.3335V5.03696L8.0058 1.33325ZM8.00516 3.23909L3.00016 6.01742V12.9999H13.0002V6.01659L8.00516 3.23909Z"
-                />
-              </svg>
-            )}
+            icon={(props) => <BathroomIcon {...props} />}
             onClick={() => {}}
             variant="secondary"
           >
             Secondary button
           </Button>
-          <Button
-            variant="secondary"
-            disabled
-            icon={(props) => (
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M8.0058 1.33325L14.6668 5.03696V14.6666H1.3335V5.03696L8.0058 1.33325ZM8.00516 3.23909L3.00016 6.01742V12.9999H13.0002V6.01659L8.00516 3.23909Z"
-                />
-              </svg>
-            )}
-          >
+          <Button variant="secondary" disabled icon={(props) => <BathroomIcon {...props} />}>
             Disabled button
           </Button>
           <Button variant="secondary" loading>
