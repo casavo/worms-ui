5ddbb8ef0527b01de690ed27710a20bea7c45d04
Laura Marina Bianchi <lauramarinabianchi@gmail.com>
2023-03-20 12:32:45 +0100

    feat: add spinner story

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -3,6 +3,7 @@ export { Button } from './components/Button';
 export { Card } from './components/Card';
 export { Separator } from './components/Separator';
 export { Text } from './components/Text';
+export { Spinner } from './components/Spinner';
 
 export { themeClass as wormsUiThemeClass, vars as theme } from './theme/theme.css';
 export * from './tokens';
